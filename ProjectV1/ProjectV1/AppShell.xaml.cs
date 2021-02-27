55b68c6c92f77e47251ec06c013209f312e62899
BeardyTheBear <55627116+BeardyTheBear@users.noreply.github.com>
2021-02-27 08:36:49 +0000

    First commit

---
+++ ProjectV1/ProjectV1/AppShell.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ProjectV1
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
